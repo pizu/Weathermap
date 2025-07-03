27f40d3f8522f5d397d30b64c3031bb0891e9193
pizu <pizu@users.noreply.github.com>
2025-07-03 16:30:34 +0200

    Add PHPUnit tests for render_colour

---
+++ tests/RenderColourTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../lib/WeatherMap.functions.php';
+
+class RenderColourTest extends TestCase
+{
+    public function testNone()
+    {
+        $this->assertEquals('none', render_colour([-1,-1,-1]));
+    }
+
+    public function testCopy()
+    {
+        $this->assertEquals('copy', render_colour([-2,-2,-2]));
+    }
+
+    public function testContrast()
+    {
+        $this->assertEquals('contrast', render_colour([-3,-3,-3]));
+    }
+
+    public function testOther()
+    {
+        $this->assertEquals('1 2 3', render_colour([1,2,3]));
+    }
+}
