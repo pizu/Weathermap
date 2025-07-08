45d920b5ba5cf538edca9fbc528d0bbc114094f4
pizu <pizu@users.noreply.github.com>
2025-07-08 10:16:25 +0200

    Update MapTimestampTest.php

--- tests/MapTimestampTest.php
+++ tests/MapTimestampTest.php
@@ -12,4 +12,14 @@ class MapTimestampTest extends TestCase
         $wm->DrawMap('null');
         $this->assertEquals('2010', $wm->datestamp);
     }
+
+    public function testManualDatestampUnchanged()
+    {
+        $wm = new WeatherMap();
+        $image = imagecreatetruecolor(100, 100);
+        $colour = imagecolorallocate($image, 0, 0, 0);
+        $wm->datestamp = 'custom';
+        $wm->DrawTimestamp($image, 2, $colour);
+        $this->assertEquals('custom', $wm->datestamp);
+    }
 }
